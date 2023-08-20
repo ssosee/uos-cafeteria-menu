555374d22540c01230b6c0838a8c5c297b202629
howisitgoing <howisitgoing@kakao.com>
2023-08-21 01:33:08 +0900

    이슈: 식당 운영시간 변경 1. 예외 메시지 변경

--- src/main/java/seaung/uoscafeteriamenu/web/exception/UosRestaurantMenuException.java
+++ src/main/java/seaung/uoscafeteriamenu/web/exception/UosRestaurantMenuException.java
@@ -5,8 +5,10 @@ public class UosRestaurantMenuException extends IllegalArgumentException {
     public static String NOT_FOUND_MENU = "예외 발생.... 학교에서 메뉴를 제공하지 않았다.. 휴.먼.친구.";
     public static String NOT_FOUND_RESTAURANT = "예외 발생.... 지원하지 않는 식당이다. 휴.먼.친구.";
     public static String NOT_FOUND_MEAL_TYPE = "예외 발생.... 지원하지 않는 식사종류다. 휴.먼.친구.";
-    public static String NOT_PROVIDE_MENU_AT_WEEKEND = "삐..삐비빅..\n주말에는 나도 쉰다. 휴.먼...\n우.리 평일에 만나자.";
-    public static String CLOSED = "모든 학교식당 영업이 종료되었다.. 휴.먼\n내일 만나자 휴.먼\n\n\n조식: 09:00 ~ 11:00\n중식: 11:00 ~ 14:00\n석식: 17:00 ~ 18:30";
+    public static String NOT_PROVIDE_MENU_AT_WEEKEND = "삐..삐비빅..\n주말에는 나도 에너.지 충전한다. 휴.먼...\n우.리 평일에 만나자.";
+    public static String CLOSED = "모든 학.교식당 소속 휴먼들이 에.너지.를 충.전하.러 갔다.\n우.리 내일. 만나자 휴.먼" +
+            "\n\n\n조식: 08:30 ~ 11:00\n중식: 11:00 ~ 14:00\n석식: 17:00 ~ 18:30" +
+            "\n\n식당별.로 운영시.간은 약간의 차이가 있을 수. 있다.";
 
     public UosRestaurantMenuException() {
         super();
