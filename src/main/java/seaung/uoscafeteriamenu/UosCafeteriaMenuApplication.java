a09e3005ce1b16dd69c73dc817fb1b90090f643e
howisitgoing <howisitgoing@kakao.com>
2024-07-18 22:29:53 +0900

    feat(sentry 설정): v1.0.11

--- src/main/java/seaung/uoscafeteriamenu/UosCafeteriaMenuApplication.java
+++ src/main/java/seaung/uoscafeteriamenu/UosCafeteriaMenuApplication.java
@@ -4,17 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import seaung.uoscafeteriamenu.crawling.crawler.Crawler;
-import seaung.uoscafeteriamenu.crawling.service.CrawlingUosRestaurantService;
-import seaung.uoscafeteriamenu.domain.repository.ApiUserMemberRepository;
-import seaung.uoscafeteriamenu.domain.repository.ApikeyRepository;
-import seaung.uoscafeteriamenu.domain.repository.CrawlingTargetRepository;
-import seaung.uoscafeteriamenu.domain.repository.SkillBlockRepository;
 
 @Slf4j
 @EnableCaching
