5e9462f93de9b0da490950de83800d91327449b8
howisitgoing <howisitgoing@kakao.com>
2024-09-28 22:24:19 +0900

    migration(): v1.1.11
    - 서버 마이그레이션

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM openjdk:11
 ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} app.jar
 ENV TZ Asia/Seoul
-ENV APP_VERSION=v1.0.11
+ENV APP_VERSION=v1.1.11
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/seaung/uoscafeteriamenu/domain/cache/repository/CacheUosRestaurantRepository.java
+++ src/main/java/seaung/uoscafeteriamenu/domain/cache/repository/CacheUosRestaurantRepository.java
@@ -1,14 +1,11 @@
 package seaung.uoscafeteriamenu.domain.cache.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import seaung.uoscafeteriamenu.domain.cache.entity.CacheUosRestaurant;
 import seaung.uoscafeteriamenu.domain.entity.MealType;
 import seaung.uoscafeteriamenu.domain.entity.UosRestaurantName;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface CacheUosRestaurantRepository extends CrudRepository<CacheUosRestaurant, String> {
 
