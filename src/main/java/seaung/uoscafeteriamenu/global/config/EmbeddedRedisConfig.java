04ccae9d74b7159acf4a95ae886646b8e72e3d20
howisitgoing <howisitgoing@kakao.com>
2025-11-09 20:18:09 +0900

    fix(EmbeddedRedisConfig, build.gradle, cicd-light-sail.yml): enhance compatibility and update configurations
    - Updated `embedded-redis` to version 0.7.3.
    - Enabled `EmbeddedRedisConfig` for `prod` profile and added support for x86 Mac systems.
    - Refactored constants and port selection logic in `EmbeddedRedisConfig`.
    - Adjusted Docker run command in `cicd-light-sail.yml` to use `--network host`.
    - Added binary for Redis server on x86 Mac.

--- src/main/java/seaung/uoscafeteriamenu/global/config/EmbeddedRedisConfig.java
+++ src/main/java/seaung/uoscafeteriamenu/global/config/EmbeddedRedisConfig.java
@@ -25,20 +25,29 @@ import java.io.InputStreamReader;
  */
 @Slf4j
 @Configuration
-@Profile({"test", "local"})
+@Profile({"test", "local", "prod"})
 public class EmbeddedRedisConfig {
+
+    private static final int START_PORT = 10_000;
+    private static final int END_PORT = 65_535;
+    private static final String FIND_PORT_COMMAND = "netstat -nat | grep LISTEN|grep %d";
+    private static final String BASH = "/bin/sh";
+    private static final String C = "-c";
+    private static final String NOT_FOUND_AVAILABLE_PORT_MESSAGE = "Not Found Available port: %d ~ %d";
+    private static final String REDIS_SERVER_START_MESSAGE = "redisServer start..={}";
+    private static final String REDIS_SERVER_STOP_MESSAGE = "redisServer stop..";
 
     @Value("${spring.redis.port}")
     private int redisPort;
     private RedisServer redisServer;
-    private static final String REDIS_SERVER_START_MESSAGE = "redisServer start..={}";
-    private static final String REDIS_SERVER_STOP_MESSAGE = "redisServer stop..";
 
     @PostConstruct
     public void runRedis() throws IOException {
         int port = isRedisRunning() ? findAvailablePort() : redisPort;
         if (isArmMac()) {
             redisServer = new RedisServer(getRedisFileForArcMac(), port);
+        } else if (isX86Mac()) {
+            redisServer = new RedisServer(getRedisFileForX86Mac(), port);
         } else {
             redisServer = RedisServer.builder()
                     .port(port)
@@ -53,7 +62,7 @@ public class EmbeddedRedisConfig {
 
     @PreDestroy
     public void stopRedis() {
-        if(!ObjectUtils.isEmpty(redisServer)) {
+        if (!ObjectUtils.isEmpty(redisServer)) {
             redisServer.stop();
             log.info(REDIS_SERVER_STOP_MESSAGE);
         }
@@ -71,22 +80,23 @@ public class EmbeddedRedisConfig {
      */
     public int findAvailablePort() throws IOException {
 
-        for (int port = 10000; port <= 65535; port++) {
+        for (int port = START_PORT; port <= END_PORT; port++) {
             Process process = executeGrepProcessCommand(port);
             if (!isRunning(process)) {
                 return port;
             }
         }
 
-        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+        throw new IllegalArgumentException(String.format(NOT_FOUND_AVAILABLE_PORT_MESSAGE, START_PORT, END_PORT));
     }
 
     /**
      * 해당 port를 사용중인 프로세스 확인하는 sh 실행
      */
     private Process executeGrepProcessCommand(int port) throws IOException {
-        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
-        String[] shell = {"/bin/sh", "-c", command};
+        String command = String.format(FIND_PORT_COMMAND, port);
+        String[] shell = {BASH, C, command};
+
         return Runtime.getRuntime().exec(shell);
     }
 
@@ -119,11 +129,31 @@ public class EmbeddedRedisConfig {
     }
 
     /**
+     * 현재 시스템이 X86 아키텍처를 사용하는 MAC인지 확인 System.getProperty("os.arch") : JVM이 실행되는 시스템 아키텍처 반환
+     * System.getProperty("os.name") : 시스템 이름 반환
+     */
+    private boolean isX86Mac() {
+        return Objects.equals(System.getProperty("os.arch"), "x86_64")
+                && Objects.equals(System.getProperty("os.name"), "Mac OS X");
+    }
+
+    /**
      * ARM 아키텍처를 사용하는 Mac에서 실행할 수 있는 Redis 바이너리 파일을 반환
      */
     private File getRedisFileForArcMac() {
         try {
             return new ClassPathResource("redis/redis-server-mac-arm64").getFile();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    /**
+     * X86 아키텍처를 사용하는 Mac에서 실행할 수 있는 Redis 바이너리 파일을 반환
+     */
+    private File getRedisFileForX86Mac() {
+        try {
+            return new ClassPathResource("redis/redis-server-mac-x86").getFile();
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
